eea5c7e1e346bf7fec48a9f9003e5ac88f4a9ace
Blessy2006 <68602797+Blessy2006@users.noreply.github.com>
2020-08-29 18:16:34 +0530

    Add files via upload

--- Box.js
+++ Box.js
@@ -12,15 +12,19 @@ class Box {
   World.add(world, this.body);
 }       
 display(){
-  var angle = this.body.angle;
-  push();
-  translate(this.body.position.x, this.body.position.y);
-  rotate(angle);
-  rectMode(CENTER);
-  image(pos.x, pos.y, this.width, this.height);
-  pop();
-}
+  console.log(this.body.speed);
+   if(this.body.speed < 3){
+    super.display();
+   }
+   else{
+     World.remove(world, this.body);
+     push();
+     this.Visiblity = this.Visiblity - 5;
+     tint(255,this.Visiblity);
+     image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+     pop();
+   }
    
   }
 
-
+}
